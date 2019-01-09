8ab60efc16738c9236cc3eaa0bbc045b8c2b972b
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-01-09 16:47:16 -0600

    get server live

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ const bodyParser = require('body-parser');
 // initialize app as an instance of Express, the framework
 const app = express();
 
+// to get server running, tell app to listen for HTTP requests
+const port = 8000;
+
+app.listen(port, () => {
+	console.log('we are live on port ' + port);
+}); // run npm run dev to see if it worked!
+
