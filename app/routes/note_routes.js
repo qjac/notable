bf4250921faf17f47ff0a73a54bd88bd6e9b2848
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-01-10 13:44:03 -0600

    finish create route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,15 @@
 module.exports = function(app, db) {
+	const collection = 
+
 	app.post('/notes', (req,res) => {
 		// create note here
-		console.log(req.body);
-		res.send('hello');
+		const note = { text: req.body.text, title: req.body.title};
+		db.collection('notes').insert(note, (err,result) => {
+			if (err) {
+				res.send({ 'error': 'An error has occured' });
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
 	});
 };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // start by requiring dependencies
-const express =require('express');
+const express = require('express');
 // use the MongoClient to interact with your database
 // from so: MongoClient in your example is just a Nodejs library that handles connecting to and interacting with a MongoDB database. 
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 // require that new db you set up
-const db =require('./config/db');
+let db = require('./config/db');
 
 // initialize app as an instance of Express, the framework
 const app = express();
@@ -17,7 +17,7 @@ const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // NOW CONNECT YOUR NEW DB
-MongoClient.CONNECT(DB.URL, (ERR, database) => {
+MongoClient.connect(db.url, (err, database) => {
 	if (err)  return console.log(err);
 	// import new routes for use
 	db = database.db('notable-demo');
