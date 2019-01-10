e70e90ee9f9924bd0214627fa3372593a992bb1c
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-01-10 13:54:51 -0600

    create read route

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,25 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-	const collection = 
+	// read
+	app.get('/notes/:id', (req, res) => {
+		const id =req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error': 'An error has occured. Could not read.' });
+			} else {
+				res.send(item);
+			}
+		});
+	});
 
 	app.post('/notes', (req,res) => {
 		// create note here
 		const note = { text: req.body.text, title: req.body.title};
 		db.collection('notes').insert(note, (err,result) => {
 			if (err) {
-				res.send({ 'error': 'An error has occured' });
+				res.send({ 'error': 'An error has occured. Could not create.' });
 			} else {
 				res.send(result.ops[0]);
 			}
